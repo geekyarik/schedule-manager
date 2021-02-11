ed31d0560b3fb1a003e9f466b8f7755ef4e0a739
Yaroslav <y380979684368@gmail.com>
2021-02-11 16:09:38 +0200

    Added spring security. Separated database module configuration in properties file.

--- schedule-manager-db/src/main/java/com/ystan/schedule/DatabaseModuleConfig.java
+++ schedule-manager-db/src/main/java/com/ystan/schedule/DatabaseModuleConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -16,10 +17,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = DatabaseModuleConfig.BASE_PACKAGE)
 @ComponentScan(basePackages = DatabaseModuleConfig.BASE_PACKAGE)
 @EnableJpaRepositories(basePackages = DatabaseModuleConfig.BASE_PACKAGE)
+@PropertySource(value = DatabaseModuleConfig.DATABASE_PROPERTIES_SOURCE)
 public class DatabaseModuleConfig {
 
     /**
      * Base package of the DB module.
      */
     public static final String BASE_PACKAGE = "com.ystan.schedule";
+
+    /**
+     * Path to the .properties file with DB configuration.
+     */
+    public static final String DATABASE_PROPERTIES_SOURCE = "classpath:database.properties";
 }
